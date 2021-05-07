6882ea297555a288240fe46a6ea8375328b87aac
Samuferr <samuferr@gmail.com>
2021-05-07 08:40:42 -0300

    Project Created

---
+++ backend/src/main/java/com/java/dsproject/DsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.java.dsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsprojectApplication.class, args);
+	}
+
+}
